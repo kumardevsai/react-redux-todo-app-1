8febb83213c2af79faea7ba9e5ae12267fea2ec8
Mert Köseoğlu <info@mkoseoglu.com>
2018-03-03 01:52:51 +0300

    redux dev extension and postcss

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import store from './store/index'
 
 import App from './containers/App'
+import './style/app.css'
 
 render(
   <Provider store={store}>
--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,10 @@ import thunk from 'redux-thunk'
 
 import rootReducer from '../reducers/index'
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
-)
+let devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+if (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production') {
+  devTools = a => a
+}
+const store = createStore(rootReducer, compose(applyMiddleware(thunk), devTools))
 
 export default store
